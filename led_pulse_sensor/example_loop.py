352328aa73662f364339cc0848c35c017d287831
Nicklas <nicklaslundin@gmail.com>
2016-07-03 16:41:08 +0200

    clean up of example loop

--- led_pulse_sensor/example_loop.py
+++ led_pulse_sensor/example_loop.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
-from __future__ import print_function
 from Adafruit_TSL2561 import Adafruit_TSL2561
-import time
 
 # Initialise the sensor
 LightSensor = Adafruit_TSL2561()
@@ -13,10 +11,9 @@ LightSensor.enable_auto_gain(True)
 
 # Get the calculated lux value, this is a spot reading so if you're under light
 while True:
-	try:
-	    lux = LightSensor.calculate_lux()
-	except OverflowError as e:
-	    print(e)
-	else:
-	    print("Lux value is ", lux)
-    	#time.sleep(0.5)
+    try:
+        lux = LightSensor.calculate_lux()
+    except OverflowError as e:
+        print e
+    else:
+        print "Lux value is {}".format(lux)
