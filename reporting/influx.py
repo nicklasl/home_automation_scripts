47fde31addf16f50d45190a437998e00d18d7ace
Nicklas <nicklaslundin@gmail.com>
2016-09-11 07:01:33 +0200

    removed logging and fixed "loop issue"

--- reporting/influx.py
+++ reporting/influx.py
@@ -13,20 +13,19 @@ q = FifoDiskQueue("queuefile")
 
 
 def append_to_retry_queue(json):
-    print "pushing to retry queue: {}".format(json)
     q.push(str(json))
 
 
 def write_retry_queue():
     while q:
         queued_json = eval(q.pop())
-        print "trying to log a retry {}".format(queued_json)
-        log(queued_json, verbose=True, add_to_retry_queue_if_failed=False)
+        log(queued_json, verbose=True, retry = False)
         time.sleep(1)
 
 
-def log(json, verbose=False, add_to_retry_queue_if_failed=True):
-    write_retry_queue()
+def log(json, verbose=False, retry = True):
+    if retry:
+        write_retry_queue()
 
     append_time(json)
     if verbose:
@@ -34,30 +33,25 @@ def log(json, verbose=False, add_to_retry_queue_if_failed=True):
     try:
         client.write_points(json)
     except exceptions.ConnectionError as connection_error:
-        print "there was an error: {}".format(connection_error)
-        logger.error("there was an error: {}".format(connection_error))
-        if add_to_retry_queue_if_failed:
-            append_to_retry_queue(json)
+        handle_error(connection_error, json)
     except exceptions.ReadTimeout as read_timeout:
-        print "there was an error: {}".format(read_timeout)
-        logger.error("there was an error: {}".format(read_timeout))
-        if add_to_retry_queue_if_failed:
-            append_to_retry_queue(json)
+        handle_error(read_timeout, json)
     except influxdb.exceptions.InfluxDBServerError as db_error:
-        print "there was an error: {}".format(db_error)
-        logger.error("there was an error: {}".format(db_error))
-        if add_to_retry_queue_if_failed:
-            append_to_retry_queue(json)
+        handle_error(db_error, json)
     except:
         print "Unexpected error:", sys.exc_info()[0]
         raise
+
+
+def handle_error(e, json):
+    logger.error("there was an error: {}".format(e))
+    append_to_retry_queue(json)
 
 
 def append_time(json):
     nanotime = int(time.time() * 1000000000L)
     for dict in json:
         if not 'time' in dict:
-            print "appending time: {}".format(nanotime)
             dict['time'] = nanotime
 
 
