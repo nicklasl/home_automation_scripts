9f5a61eb3fb93ad44f102effb2f169456ae09dda
Nicklas Lundin <nicklaslundin@gmail.com>
2016-11-06 13:38:26 +0100

    sending a warning email with gmail.

--- alarm/alarm_watch.py
+++ alarm/alarm_watch.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
+# coding=utf-8
 import os
+import smtplib
 import sys
+from email.mime.text import MIMEText
 
 # Path hack.
 sys.path.insert(0, os.path.abspath('..'))
@@ -10,6 +13,7 @@ import config
 import logging
 
 LIMIT = 1600
+
 
 def setup_logging():
     global logger
@@ -27,13 +31,31 @@ def setup_influx_client():
     client = influxdb.InfluxDBClient(INFLUX_URI, 8086, INFLUX_USERNAME, INFLUX_PASSWORD, INFLUX_DATABASE)
 
 
+def send_warning_email(watts_last_15m):
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(config.key('GMAIL_LOGIN'), config.key('GMAIL_APP_PASSWORD'))
+
+    sender = config.key('GMAIL_FROM_ADDRESS')
+    msg = MIMEText("""more than {} w per hour. Consumed last 15 min: {}""".format(LIMIT * 4, watts_last_15m))
+    recipients = [config.key('GMAIL_TO_ADDRESS1'), config.key('GMAIL_TO_ADDRESS2')]
+    msg['Subject'] = "Kolla värmepumpen"
+    msg['From'] = sender
+    msg['To'] = ", ".join(recipients)
+
+    server.sendmail(sender, recipients, msg.as_string())
+    server.quit()
+
+
 def main():
     result = client.query('select sum(value) from electricity where time > now() - 15m')
     dict = result.raw
     watts_last_15m = dict[u'series'][0][u'values'][0][1]
     print watts_last_15m
     if watts_last_15m > LIMIT:
-        logger.warn("more than {} w per hour. Consumed last 15 min: {}".format(LIMIT * 4, watts_last_15m))
+        logger.warn("Consumed {} W last 15 minutes. Limit is {}".format(watts_last_15m, LIMIT))
+        send_warning_email(watts_last_15m)
     else:
         logger.debug("Consumed last 15 min: {} W".format(watts_last_15m))
 
