37013d61c287a70a2af7a048774a751f5cc9d9eb
Nicklas <nicklaslundin@gmail.com>
2017-04-20 08:10:50 +0200

    first iteration of server

--- 1wire_temperature_sensor/server/1wire_web_server.py
+++ 1wire_temperature_sensor/server/1wire_web_server.py
@@ -1,11 +1,29 @@
 import os
+import json
+import time
 from flask import Flask
+import sensor_reader
 app = Flask(__name__)
 
+BASE_DIR = '/sys/bus/w1/devices/'
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/sensors")
+def list_sensors():
+    dirs = [d for d in os.listdir(BASE_DIR) if os.path.isdir(os.path.join(BASE_DIR, d))]
+    return json.dumps(dirs)
+
+@app.route("/sensors/<id>")
+def get_temp_for_sensor(id):
+    temperature = sensor_reader.read_temp(BASE_DIR+id)
+    result = {"id": id,
+     "temperature": temperature,
+     "timestamp": time.time()}
+    return json.dumps(result)
+
 
 if __name__ == "__main__":
     if os.getuid() != 0:
