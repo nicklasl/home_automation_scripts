267db9d92f91d5698c420ed89bcdf4b805773608
Nicklas Lundin <nicklaslundin@gmail.com>
2017-06-10 21:43:37 +0200

    clean up

--- door_sensor/door_sensor.py
+++ door_sensor/door_sensor.py
@@ -2,12 +2,16 @@ import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
-pir_pin = 18
-door_pin = 23
+#24 = Blue
+basement_door_pin = 24
+#23 = Green
+garage_door_pin = 23
 
-GPIO.setup(door_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # activate input with PullUp
-
+GPIO.setup(basement_door_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(garage_door_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
-    if GPIO.input(door_pin):
-        print("DOOR ALARM!")
-    time.sleep(0.5)
+    if GPIO.input(basement_door_pin):
+        print("BASEMENT DOOR ALARM!")
+    if GPIO.input(garage_door_pin):
+        print("GARAGE_DOOR_ALARM")
+    time.sleep(2)
