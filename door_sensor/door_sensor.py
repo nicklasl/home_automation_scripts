00de9e053092f9354213971b2e9688274216ae7d
Nicklas Lundin <nicklaslundin@gmail.com>
2017-06-10 21:04:53 +0200

    removed not used gpio

--- door_sensor/door_sensor.py
+++ door_sensor/door_sensor.py
@@ -8,8 +8,6 @@ door_pin = 23
 GPIO.setup(door_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # activate input with PullUp
 
 while True:
-    if GPIO.input(pir_pin):
-        print("PIR ALARM!")
     if GPIO.input(door_pin):
         print("DOOR ALARM!")
     time.sleep(0.5)
