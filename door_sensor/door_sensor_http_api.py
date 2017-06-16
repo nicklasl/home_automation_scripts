9d593d9bdae9eb01083655dc0e746c42c91ad389
Nicklas Lundin <nicklaslundin@gmail.com>
2017-06-16 19:18:40 +0200

    http api test

---
+++ door_sensor/door_sensor_http_api.py
@@ -0,0 +1,33 @@
+import json
+
+import yaml
+from flask import Flask
+from gpiozero import Button
+
+app = Flask(__name__)
+
+basement_door_pin = 24
+garage_door_pin = 23
+garage = Button(basement_door_pin)
+basement = Button(garage_door_pin)
+
+
+@app.route("/state")
+def get_state():
+    return json.dumps([
+        {'door': cfg['door'][basement_door_pin],
+         'state': basement.is_pressed},
+        {'door': cfg['door'][garage_door_pin],
+         'state': garage.is_pressed}
+    ])
+
+
+def load_cfg():
+    global cfg
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+
+
+if __name__ == "__main__":
+    load_cfg()
+    app.run(host='0.0.0.0', port=5001)
