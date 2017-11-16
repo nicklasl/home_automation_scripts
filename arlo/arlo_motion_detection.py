861caf08bed7b1c7b93ad2660d62aa459528a5c8
Nicklas Lundin <nicklaslundin@gmail.com>
2017-11-16 21:07:21 +0100

    run sh script when motion detected

--- arlo/arlo_motion_detection.py
+++ arlo/arlo_motion_detection.py
@@ -1,26 +1,25 @@
 from Arlo import Arlo
 import config
+import os
+from subprocess import call
 
 USERNAME = config.key("ARLO_USERNAME")
 PASSWORD = config.key("ARLO_PASSWORD")
 
+MOTION_DETECT_ACTION_SCRIPT = "curl.sh"
 
 try:
-
-    # Instantiating the Arlo object automatically calls Login(), which returns an oAuth token that gets cached.
-    # Subsequent successful calls to login will update the oAuth token.
     arlo = Arlo(USERNAME, PASSWORD)
-    # At this point you're logged into Arlo.
-
-    # Get the list of devices and filter on device type to only get the basestation.
-    # This will return an array which includes all of the basestation's associated metadata.
     basestations = arlo.GetDevices('basestation')
 
     # Define a callback function that will get called once for each motion event.
     def callback(arlo, basestation, event):
         # Here you will have access to self, basestation_id, xcloud_id, and the event schema.
         print("motion event detected!")
-        #print(event)
+        print(event)
+        print(basestation)
+        if os.path.isfile(MOTION_DETECT_ACTION_SCRIPT):
+            call(['bash', MOTION_DETECT_ACTION_SCRIPT, "camera id"])
         #print(arlo)
 
     # Subscribe to motion events. This method blocks until the event stream is closed. (You can close the event stream in the callback if you no longer want to listen for events.)
