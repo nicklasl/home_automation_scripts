0dde088696ec70e1205c5ae2bbc6ba0eef20f547
Nicklas Lundin <nicklaslundin@gmail.com>
2018-02-13 19:27:20 +0100

    removed value diff

--- dht_humitemp_sensor/dht_web_server.py
+++ dht_humitemp_sensor/dht_web_server.py
@@ -37,8 +37,6 @@ def get_temp_for_sensor(id):
         return abort(404)
 
     temperature, humidity = sensor_reader.read_temp_and_humidity(sensor_to_check["gpioPin"])
-    if id == "2": #fix value diff
-        temperature += 2
     result = {"id": id,
               "friendlyName": sensor_to_check["friendlyName"],
               "temperature": temperature,
