f3601cb1f49969533cc9ce2139d150f35304233d
Nicklas Lundin <nicklaslundin@gmail.com>
2018-11-13 20:45:28 +0100

    added simple app to restart node-red

---
+++ pm2_restarter/pm2_restarter.py
@@ -0,0 +1,15 @@
+import os
+import json
+import time
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+@app.route("/")
+def restart():
+    os.system('pm2 restart node-red')
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', import=5001)
